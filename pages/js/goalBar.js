d46c5993a4d1d636677e746d3ddcc6a655794465
Joyce Woo <yyjoycewoo@gmail.com>
2016-11-20 08:18:36 -0500

    Adjusted font and progress bar size

--- pages/js/goalBar.js
+++ pages/js/goalBar.js
@@ -4,7 +4,8 @@
         var bar = new ProgressBar.Line('#progress', {
             color: '#336699',
             strokeWidth: 10,
-            trailWidth: 1,
+            trailColor: '#99CCFF',
+            trailWidth: 10,
             duration: 1500,
             bar: 60,
             text: {style: {
@@ -17,7 +18,7 @@
             step: function (state, bar) {
                 bar.setText((bar.value() * 100).toFixed(0) + '%');
                 target = bar.value();
-                if ((target * 100).toFixed(0) == 75) {
+                if ((target * 100).toFixed(0) == 70) {
                     bar.stop();
                 }
             }
